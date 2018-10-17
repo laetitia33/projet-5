50031a3a162a6c61b83f2a5e346979d18adaa8c1
Laetitia Bernardi <33260laetitia.bernardi@gmail.com>
2018-10-17 16:46:54 +0200

    css

--- models/Manager.php
+++ models/Manager.php
@@ -25,7 +25,8 @@ class Manager
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES   => false,
-     
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET time_zone = '+02:00'"
+
         ];
         return new PDO($dsn, $user, $pass, $opt);
     }
